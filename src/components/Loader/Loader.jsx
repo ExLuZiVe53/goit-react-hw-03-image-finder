85716e9593affa262e5ed554813a674bf44263fd
ExLuZiVe53 <imstudent27@gmail.com>
2023-10-02 12:22:38 +0200

    reload errors

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import ContentLoader from 'react-content-loader';
 
-const MyLoader = props => (
-  <ContentLoader
-    speed={2}
-    width={476}
-    height={124}
-    viewBox="0 0 476 124"
-    backgroundColor="#2d0101"
-    foregroundColor="#918d59"
-    {...props}
-  >
-    <rect x="48" y="8" rx="3" ry="3" width="88" height="6" />
-    <rect x="48" y="26" rx="3" ry="3" width="52" height="6" />
-    <rect x="0" y="56" rx="3" ry="3" width="410" height="6" />
-    <rect x="0" y="72" rx="3" ry="3" width="380" height="6" />
-    <rect x="0" y="88" rx="3" ry="3" width="178" height="6" />
-    <circle cx="20" cy="20" r="20" />
-  </ContentLoader>
-);
+const XYChart = props => {
+  return (
+    <ContentLoader viewBox="0 0 180 400" height={400} width={180} {...props}>
+      <rect x="20" y="5" rx="0" ry="0" width="1" height="170" />
+      <rect x="20" y="175" rx="0" ry="0" width="360" height="1" />
+
+      <rect x="40" y="75" rx="0" ry="0" width="35" height="100" />
+      <rect x="80" y="125" rx="0" ry="0" width="35" height="50" />
+      <rect x="120" y="105" rx="0" ry="0" width="35" height="70" />
+      <rect x="160" y="35" rx="0" ry="0" width="35" height="140" />
+      <rect x="200" y="55" rx="0" ry="0" width="35" height="120" />
+      <rect x="240" y="15" rx="0" ry="0" width="35" height="160" />
+      <rect x="280" y="135" rx="0" ry="0" width="35" height="40" />
+      <rect x="320" y="85" rx="0" ry="0" width="35" height="90" />
+    </ContentLoader>
+  );
+};
 
 export default MyLoader;
